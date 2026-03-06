7f862ed95781372df59091003cc4593e95cc28de
Tema817 <temashatov@gmail.com>
2026-03-06 08:31:44 +0200

    Update Footer.tsx

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,12 +6,10 @@ export default function Footer() {
             <div className={css.content}>
                 <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
                 <div className={css.wrap}>
-                    <p>Developer: Valentyna</p>
+                    <p>Developer:</p>
                     <p>
                         Contact us:
-                        <a href="https://github.com/Valentyna877"
-                                 target="_blank"
-                            rel="noopener noreferrer">
+                        <a href="/">
                             GitHub
                         </a>
                     </p>
@@ -19,4 +17,5 @@ export default function Footer() {
             </div>
         </footer>
     );
+
 }
